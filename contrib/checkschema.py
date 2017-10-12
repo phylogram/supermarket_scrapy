08f8e00aca98dbca1b2e26002587291b3aefc508
Alex Berger <alex@more-onion.com>
2017-10-12 17:11:25 +0200

    checkschema: use click

--- contrib/checkschema.py
+++ contrib/checkschema.py
@@ -1,12 +1,20 @@
+import click
 import json
 import jsonschema
 
-schema = {}
-with open('schema.json', 'r') as stream:
-    schema = json.load(stream)
 
-data = {}
-with open('testdata.json', 'r') as stream:
-    data = json.load(stream)
+@click.command()
+def validate():
+    schema = {}
+    with open('schema.json', 'r') as stream:
+        schema = json.load(stream)
 
-jsonschema.validate(data, schema)
+    data = {}
+    with open('testdata.json', 'r') as stream:
+        data = json.load(stream)
+
+    jsonschema.validate(data, schema)
+
+
+if __name__ == '__main__':
+    validate()
